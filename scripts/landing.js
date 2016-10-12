09341861dac1a43cf82a4c318dba671d5b9b9ec9
Andrea Hinkle <andreahinkle@gmail.com>
2016-10-12 18:10:31 -0500

    scroll issues

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,7 +1,7 @@
-var animatePoints = function() {
+var pointsArray = document.getElementsByClassName('point');
  
-                 var points = document.getElementsByClassName('point');
- 
+ var animatePoints = function(points) {
+     
                  var revealFirstPoint = function() {
                      points[0].style.opacity = 1;
                      points[0].style.transform = "scaleX(1) translateY(0)";
@@ -30,3 +30,10 @@ var animatePoints = function() {
              };
 
 animatePoints()
+
+window.onload = function() {
+     var sellingPoints = document.getElementsByClassName('selling-points')[0];
+     window.addEventListener("scroll", function(event) {
+         console.log("Current offset from the top is " + sellingPoints.getBoundingClientRect().top + " pixels");
+     });
+ }
