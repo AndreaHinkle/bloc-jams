3e30a07e845f9a0b92210c7bdd4adef1da8302fc
Andrea Hinkle <andreahinkle@gmail.com>
2016-10-15 11:00:59 -0500

    fixed checkpoint 22 issues

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,39 +1,30 @@
-var pointsArray = document.getElementsByClassName('point');
+var animatePoints = function() {
  
- var animatePoints = function(points) {
-     
-                 var revealFirstPoint = function() {
-                     points[0].style.opacity = 1;
-                     points[0].style.transform = "scaleX(1) translateY(0)";
-                     points[0].style.msTransform = "scaleX(1) translateY(0)";
-                     points[0].style.WebkitTransform = "scaleX(1) translateY(0)";
-                 };
+     var points = document.getElementsByClassName('point');
  
-                 var revealSecondPoint = function() {
-                     points[1].style.opacity = 1;
-                     points[1].style.transform = "scaleX(1) translateY(0)";
-                     points[1].style.msTransform = "scaleX(1) translateY(0)";
-                     points[1].style.WebkitTransform = "scaleX(1) translateY(0)";
-                 };
+     var revealFirstPoint = function() {
+         points[0].style.opacity = 1;
+         points[0].style.transform = "scaleX(1) translateY(0)";
+         points[0].style.msTransform = "scaleX(1) translateY(0)";
+         points[0].style.WebkitTransform = "scaleX(1) translateY(0)";
+     };
  
-                 var revealThirdPoint = function() {
-                     points[2].style.opacity = 1;
-                     points[2].style.transform = "scaleX(1) translateY(0)";
-                     points[2].style.msTransform = "scaleX(1) translateY(0)";
-                     points[2].style.WebkitTransform = "scaleX(1) translateY(0)";
-                 };
+     var revealSecondPoint = function() {
+         points[1].style.opacity = 1;
+         points[1].style.transform = "scaleX(1) translateY(0)";
+         points[1].style.msTransform = "scaleX(1) translateY(0)";
+         points[1].style.WebkitTransform = "scaleX(1) translateY(0)";
+     };
  
-                 revealFirstPoint();
-                 revealSecondPoint();
-                 revealThirdPoint();
+     var revealThirdPoint = function() {
+         points[2].style.opacity = 1;
+         points[2].style.transform = "scaleX(1) translateY(0)";
+         points[2].style.msTransform = "scaleX(1) translateY(0)";
+         points[2].style.WebkitTransform = "scaleX(1) translateY(0)";
+     };
  
-             };
-
-animatePoints()
-
-window.onload = function() {
-     var sellingPoints = document.getElementsByClassName('selling-points')[0];
-     window.addEventListener("scroll", function(event) {
-         console.log("Current offset from the top is " + sellingPoints.getBoundingClientRect().top + " pixels");
-     });
- }
+     revealFirstPoint();
+     revealSecondPoint();
+     revealThirdPoint();
+ 
+ };
